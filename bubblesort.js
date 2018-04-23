55dcd9a2113593b4687ede916d3cbed4738cdfb8
Jami Gibbs <jami0821@gmail.com>
2018-04-23 14:00:03 -0500

    Initial

---
+++ bubblesort.js
@@ -0,0 +1,38 @@
+function bubbleSort(arr) {
+  const counter = arr.length;
+  let ticks = 0;
+  if (!Array.isArray(arr)) return;
+  while (ticks < counter) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        // swapping elements
+        const temp = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = temp;
+      }
+    }
+    ticks++;
+  }
+  return arr;
+}
+
+function bubbleSortRecursive(arr, counter){
+  counter = counter || arr.length;
+
+  if (counter === 1 ){
+    return arr;
+  }
+
+  for (let i = 0; i < counter; i++) {
+    if (arr[i] > arr[i + 1]) {
+      // swapping elements
+      const temp = arr[i + 1];
+      arr[i + 1] = arr[i];
+      arr[i] = temp;
+    }
+  }
+
+  console.log(counter, arr);
+
+  return bubbleSortRecursive(arr, counter - 1);
+}
---
+++ bubblesort.spec.js
@@ -0,0 +1,33 @@
+describe('Bubble Sort', function() {
+  it('calls itself n times (iterative)', function() {
+    const arr = [5, 3, 10, 2, 8, 7];
+    spyOn(window, 'bubbleSort').and.callThrough();
+    bubbleSort(arr);
+    expect(bubbleSort.calls.count()).toEqual(1);
+  });
+
+  it('handles empty array', function() {
+    expect(bubbleSort([])).toEqual([]);
+  });
+  it('handles non-array argumnet', function() {
+    expect(bubbleSort('hello world')).toEqual(undefined);
+  });
+  it('sorts array from lowest to highest', function() {
+    expect(bubbleSort([4, 5, 10, 3, 1])).toEqual([1, 3, 4, 5, 10]);
+  });
+  it('handles single element array', function() {
+    expect(bubbleSort([4])).toEqual([4]);
+  });
+
+  it('calls itself n times (recursive)', function() {
+    const arr = [5, 3, 10, 2, 8, 7];
+    spyOn(window, 'bubbleSortRecursive').and.callThrough();
+    bubbleSortRecursive(arr);
+    expect(bubbleSortRecursive.calls.count()).toEqual(6);
+  });
+
+  it('sorts array from lowest to highest', function() {
+    expect(bubbleSortRecursive([4, 5, 10, 3, 1])).toEqual([1, 3, 4, 5, 10]);
+  });
+
+});
